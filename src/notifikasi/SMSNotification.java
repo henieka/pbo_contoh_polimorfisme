160578fb42f2654bef8c719995f2b5407534d6bf
henieka <henieka32@gmail.com>
2021-01-18 20:52:40 +0700

    first commit

---
+++ src/notifikasi/SMSNotification.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notifikasi;
+
+/**
+ *
+ * @author lenovo
+ */
+public class SMSNotification implements InterfaceNotifikasi{
+    
+    @Override
+
+    public void sendMessage(String receiver, String content) {
+    System.out.println("Mengirim SMS ke " + receiver + " dengan isi:");
+    System.out.println(content);
+    }
+    
+}
